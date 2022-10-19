ae91c95a02f9b510b600fe98f5475316e7be1665
David Haberthür <email@davidhaberthuer.ch>
2022-10-19 21:18:53 +0200

    One word per line, several strips:

--- content/uniquewords.py
+++ content/uniquewords.py
@@ -11,9 +11,12 @@ for c, f in enumerate(files):
     words = input_file.read().split()
     for d, word in enumerate(words):
         for e, punctuation in enumerate(string.punctuation):
-            word = word.replace(punctuation,'')
+            word = word.strip(punctuation)
+        for e, punctuation in enumerate(string.punctuation):
+            word = word.strip(punctuation)
         if len(word) > 5 and not word.startswith('doi') and not word.startswith('https'):
             word_list.append(word.lower())
 # Only return the unique words
 unique_words = set(word_list)
-print(sorted(unique_words))
+for uw in sorted(unique_words):
+    print(uw)
